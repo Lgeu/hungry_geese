aab6976e0486e0418738aa38d8cbec540fe0e118
Lgeu <nagiss000@gmail.com>
2021-07-25 18:15:45 +0900

    use UCB1

--- KKT89/src/Duct.cpp
+++ KKT89/src/Duct.cpp
@@ -352,9 +352,42 @@ bool Duct::Node::Expanded() const {
 }
 
 float Duct::Node::Argvalue(const int& idx_agent, const int& idx_move, const int& t_sum) {
+    // UCB1
+    
+    constexpr static float c = 1.41421356f;
+    auto res = (3.0f + GetWorth()[idx_agent][idx_move]) / (1.0f + (float)n[idx_agent][idx_move]);
+    if ((t_sum + idx_agent) % 4 != 0) res += c * std::sqrt(std::log((float)(1 + t_sum)) / (float)(1 + n[idx_agent][idx_move]));
+    return res;
+    
+    // mean UCB1-AlphaZero
+    /*
+    constexpr static float c = 1.41421356f;
+    constexpr static float c_puct = 1.0f;
+    auto res = (3.0f + GetWorth()[idx_agent][idx_move]) / (1.0f + (float)n[idx_agent][idx_move]);
+    if ((t_sum + idx_agent) % 4 != 0) res += 0.5 * c * std::sqrt(std::log((float)(1 + t_sum)) / (float)(1 + n[idx_agent][idx_move]));
+    res += 0.5 * c_puct * GetPolicy()[idx_agent][idx_move] * std::sqrt((float)t_sum) / (float)(1 + n[idx_agent][idx_move]);
+    return res;
+    */
+    
+    // alternate UCB1-AlphaZero
+    /*
+    constexpr static float c = 1.41421356f;
+    constexpr static float c_puct = 1.0f;
+    auto res = (3.0f + GetWorth()[idx_agent][idx_move]) / (1.0f + (float)n[idx_agent][idx_move]);
+    if ((((t_sum >> 2) ^ idx_agent) & 3) < 2) {
+        if ((t_sum + idx_agent) % 4 != 0) res += c * std::sqrt(std::log((float)(1 + t_sum)) / (float)(1 + n[idx_agent][idx_move]));
+    }else{
+        res += c_puct * GetPolicy()[idx_agent][idx_move] * std::sqrt((float)t_sum) / (float)(1 + n[idx_agent][idx_move]);
+    }
+    return res;
+    */
+    
+    // AlphaZero
+    /*
     constexpr float c_puct = 1.0f;
     return (3.0f + GetWorth()[idx_agent][idx_move]) / (1.0f + (float)n[idx_agent][idx_move]) 
         + c_puct * GetPolicy()[idx_agent][idx_move] * std::sqrt((float)t_sum) / (float)(1 + n[idx_agent][idx_move]);
+    */
 }
 
 // 7/24修正：返り値が{k番目の子, k番目のposition_id}に
